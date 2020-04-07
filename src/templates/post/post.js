dcaa8c995c4a00732b4885cca6b82f8dffad02ad
brent <brent.e.meyer@gmail.com>
2020-04-07 04:28:39 +0000

    adds styles for post template and other minor tweaks

--- src/templates/post/post.js
+++ src/templates/post/post.js
@@ -4,23 +4,27 @@ import { graphql } from "gatsby"
 import Layout from '../../components/layout/layout.js'
 import SEO from '../../components/seo/seo.js'
 
+import PostsListItem from '../../components/PostsListItem/PostsListItem.js';
+
+import styles from './post.module.css';
+
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
+
+  // fake the object PostsListItem requires to do its thing
+  const post = { node: { excerpt: '', id: 0, frontmatter: frontmatter } };
   return (
     <Layout>
       <SEO title={ frontmatter.title } />
-      <div className="blog-post-container">
-        <div className="blog-post">
-          <h1>{frontmatter.title}</h1>
-          <h2>{frontmatter.date}</h2>
-          <div
-            className="blog-post-content"
-            dangerouslySetInnerHTML={{ __html: html }}
-          />
-        </div>
+      <div className={ styles.postWrapper }>
+        <PostsListItem {...post} />
+        <div
+          className={ styles.postWrapperInner }
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
       </div>
     </Layout>
   )
