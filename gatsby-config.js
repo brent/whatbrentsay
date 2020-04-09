dab572e13359698724c33669509fbd8a1489e157
brent <brent.e.meyer@gmail.com>
2020-04-08 23:26:58 -0400

    adds remark image plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,26 @@ module.exports = {
 	name: `posts`,
       },
     },
-    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+	plugins: [
+	  // gatsby-remark-relative-images must
+	  // go before gatsby-remark-images
+	  {
+	    resolve: `gatsby-remark-relative-images`,
+	  },
+	  {
+	    resolve: `gatsby-remark-images`,
+	    options: {
+	      // It's important to specify the maxWidth (in pixels) of
+	      // the content container as this plugin uses this as the
+	      // base for generating different widths of each image.
+	      maxWidth: 672,
+	    },
+	  },
+	],
+      },
+    },
   ],
 }
