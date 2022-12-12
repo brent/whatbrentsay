aaa5ef36bfff8539ff65f2fe29cea3a63053e91c
brent <brent.e.meyer@gmail.com>
2022-12-12 02:36:18 -0500

    improves DateDivider

--- src/components/DateDivider/DateDivider.js
+++ src/components/DateDivider/DateDivider.js
@@ -1,19 +1,33 @@
 import * as React from 'react';
 import * as styles from './DateDivider.module.css';
 
+const isThisYear = (date) => {
+  const now = new Date();
+  const thisYear = now.getFullYear();
+  const dateYear = date.getFullYear();
+
+  if ((thisYear - dateYear) === 0) {
+    return true;
+  }
+
+  if ((thisYear - dateYear) > 0) {
+    return false;
+  }
+}
+
 const DateDivider = ({ date }) => {
   const dateObj = new Date(date);
-  const formattedDate = dateObj.toLocaleString('en-US', {
-    month: 'numeric',
-    day: 'numeric',
-    year: 'numeric',
-  });
-
-  console.log(styles);
+  const locale = 'en-US';
+  const month = dateObj.toLocaleString(locale, { month: 'short' });
+  const weekday = dateObj.toLocaleString(locale, { weekday: 'long' });
+  const day = dateObj.toLocaleString(locale, { day: 'numeric' });
+  const year = dateObj.toLocaleString(locale, { year: 'numeric' });
 
   return (
     <>
-      <p className={ styles.dateDivider }>{ formattedDate }</p>
+    <p className={ styles.dateDivider }>
+      <span className={ styles.dateDivider__weekday }>{weekday},</span> <span>{ month }</span> <span>{ day }</span>{ !isThisYear(dateObj) ? <span>, { year }</span> : null }
+    </p>
     </>
   )
 }
