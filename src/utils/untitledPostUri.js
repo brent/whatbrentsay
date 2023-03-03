0e8b8211e44b84aa2033410a806d72c48c8571a8
brent <brent.e.meyer@gmail.com>
2023-03-03 00:15:31 -0500

    refactors untitledPostUri

--- src/utils/untitledPostUri.js
+++ src/utils/untitledPostUri.js
@@ -1,29 +1,19 @@
 export const untitledPostUri = (postDate) => {
   const padNum = (num) => `0${num}`;
 
+  const formatDateComponent = ((dateComponent) => {
+    let value = dateComponent;
+    return value < 10 ? padNum(value) : value;
+  });
+
   const postDateObj = new Date(postDate);
 
   const year = postDateObj.getFullYear();
 
-  const month = (() => {
-    let month = postDateObj.getMonth() + 1; // zero-indexed
-    return month < 10 ? padNum(month) : month;
-  })();
-
-  const day = (() => {
-    let day = postDateObj.getDate();
-    return day < 10 ? padNum(day) : day;
-  })();
-
-  const hour = (() => {
-    let hour = postDateObj.getHours();
-    return hour < 10 ? padNum(hour) : hour;
-  })();
-
-  const min = (() => {
-    let min = postDateObj.getMinutes();
-    return min < 10 ? padNum(min) : min;
-  })();
+  const month = formatDateComponent(postDateObj.getMonth() + 1);
+  const day = formatDateComponent(postDateObj.getDate());
+  const hour = formatDateComponent(postDateObj.getHours());
+  const min = formatDateComponent(postDateObj.getMinutes());
 
   return `/${year}/${month}/${day}/${hour}${min}/`;
 }
